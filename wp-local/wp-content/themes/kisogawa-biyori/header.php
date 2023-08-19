2c3d6f875fe0a42fbe15253f49c15fbd9159df74
hayakawa-work <pub.y.hayakawa78@gmail.com>
2023-08-19 21:34:38 +0900

    [refactor]共通ヘッダー化に伴いヘッダーに用意した画像のスライドをTOP画面のみで表示させるよう対応

--- wp-local/wp-content/themes/kisogawa-biyori/header.php
+++ wp-local/wp-content/themes/kisogawa-biyori/header.php
@@ -1,21 +1,10 @@
 ﻿<header id="header">
 <!-- TOP-IMG ------------------------------------------------------------ -->
-<h1 class="logo"><a href="index.html"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/top_logo.png" alt="kisogawa-biyori"></a></h1>
+<h1 class="logo"><a href="/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/top_logo.png" alt="kisogawa-biyori"></a></h1>
 <!-- TOP-MENU ----------------------------------------------------------- -->
 		<?php breadcrumb(); ?>
 <!--- トップイメージ --->
-		<ul class="slider jsc-slider">
-			<li><p class="top_image"><img src="<?php echo get_stylesheet_directory_uri(); ?>/archives/2019/img/top.png"></p></li>
-			<li><p class="top_image"><img src="<?php echo get_stylesheet_directory_uri(); ?>/archives/2018/img/top.png"></p></li>
-			<li><p class="top_image"><img src="<?php echo get_stylesheet_directory_uri(); ?>/archives/2017/img/top.png"></p></li>
-			<li><p class="top_image"><img src="<?php echo get_stylesheet_directory_uri(); ?>/archives/2016/img/top.png"></p></li>
-			<li><p class="top_image"><img src="<?php echo get_stylesheet_directory_uri(); ?>/archives/2015/img/top.png"></p></li>
-			<li><p class="top_image"><img src="<?php echo get_stylesheet_directory_uri(); ?>/archives/2014/img/top.png"></p></li>
-			<li><p class="top_image"><img src="<?php echo get_stylesheet_directory_uri(); ?>/archives/2013/img/top.png"></p></li>
-			<li><p class="top_image"><img src="<?php echo get_stylesheet_directory_uri(); ?>/archives/2012/img/top.png"></p></li>
-			<li><p class="top_image"><img src="<?php echo get_stylesheet_directory_uri(); ?>/archives/2011/img/top.png"></p></li>
-		</ul>
-		
+		<?php if ( is_front_page() ) { top_images(); } ?>
 <!--- //トップイメージ --->
 <!--- sns --->
 		<ul class="sns">
