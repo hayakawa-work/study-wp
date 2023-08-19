e887b6eadeee6714e66c3adcf6421cd4f06b36cf
hayakawa-work <pub.y.hayakawa78@gmail.com>
2023-08-19 21:24:51 +0900

    [feat]TOP以外の画面をWPの固定ページから生成した画面に切り替える

--- wp-local/wp-content/themes/kisogawa-biyori/functions/breadcrumb.php
+++ wp-local/wp-content/themes/kisogawa-biyori/functions/breadcrumb.php
@@ -11,10 +11,10 @@ function breadcrumb() {
 	
   echo '<nav id="gnavi">
 		<ul class="gnavi_list">
-			<li><a href="sub/about/"><span>about</span></a></li>
-			<li><a href="sub/archive/"><span>archive</span></a></li>
-			<li><a href="sub/supporter/"><span>supporter</span></a></li>
-			<li><a href="sub/mail/"><span>mail</span></a></li>
+			<li><a href="/about/"><span>about</span></a></li>
+			<li><a href="/archive/"><span>archive</span></a></li>
+			<li><a href="/supporter/"><span>supporter</span></a></li>
+			<li><a href="/mail/"><span>mail</span></a></li>
 		</ul>
 	</nav>
 	';
